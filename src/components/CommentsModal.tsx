f6b3c086c10f127c0eb27780a81a8bc1958815b0
Awesome-Tiya <tiyashidas123@gmail.com>
2025-09-20 22:09:52 +0530

    new component comments modal

---
+++ src/components/CommentsModal.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Comment } from "./Comment";
+import { CommentBox } from "./CommentBox";
+
+type CommentProps = {
+    id: number;
+    content: string;
+    onUpvote?: (id: number) => void;
+    initialUpvotes: number;
+}
+
+type CommentBoxProps = {
+    content: string;
+    onChange: (value: string) => void;
+    onClick?: () => void;
+    send: React.ReactNode;
+}
+
+type CommentsModalProps = {
+    variant?: 'regular' | 'outlined';
+    comments: CommentProps[];
+    commentBox: CommentBoxProps;
+    numberOfComments: number;
+    onClick?: () => void;
+    backButton: React.ReactNode;
+}
+export const CommentsModal: React.FC<CommentsModalProps> = ({ onClick, comments, commentBox, numberOfComments, variant='regular', backButton }) => {
+
+    return (<>
+        <div className={`comments-modal ${variant}`}>
+            <div className="line">
+                <button type="button" className="back-button" onClick={onClick}>{ backButton }</button>
+                <h3 className="comments">Comments {numberOfComments}</h3>
+            </div>
+            <div className="comment-list">
+                {comments.map((comment) => (
+                <Comment key={comment.id} id={comment.id}
+                    content={comment.content} initialUpvotes={comment.initialUpvotes} onUpvote={comment.onUpvote} />))}
+            </div>
+            <div className="comment-box-container">
+                <CommentBox content={commentBox.content} onChange={commentBox.onChange} onClick={commentBox.onClick} send={commentBox.send} />
+            </div>
+        </div>
+    </>);
+}
---
+++ src/stories/CommentsModal.stories.tsx
@@ -0,0 +1,86 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { CommentsModal } from '../components/CommentsModal';
+
+const meta: Meta<typeof CommentsModal> = {
+    title: 'components/CommentsModal',
+    component: CommentsModal,
+    argTypes: {
+        variant: {
+            control: { type: 'radio' },
+            options: ["regular", "outlined"],
+        },
+        comments: { control: 'object' },
+        commentBox: { control: 'object' },
+        numberOfComments: { control: 'number' },
+        backButton: { control: 'object' },
+        onClick: { action: "clicked" }
+    },
+};
+export default meta;
+
+type Story = StoryObj<typeof CommentsModal>;
+
+export const Regular: Story = {
+    args: {
+        variant: "regular",
+        comments: [
+            {
+                id: 1,
+                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Cursus in hac habitasse platea dictumst quisque sagittis purus `,
+                initialUpvotes: 18000,
+            },
+            {
+                id: 2,
+                content: `Lorem2 ipsum dolor sit amet, consectetur adipiscing elit. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Cursus in hac habitasse platea dictumst quisque sagittis purus `,
+                initialUpvotes: 18000,
+            },
+            {
+                id: 3,
+                content: `Lorem3 ipsum dolor sit amet, consectetur adipiscing elit. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Cursus in hac habitasse platea dictumst quisque sagittis purus `,
+                initialUpvotes: 18000,
+            },
+            {
+                id: 4,
+                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Cursus in hac habitasse platea dictumst quisque sagittis purus Lorem4 ipsum dolor sit amet, consectetur adipiscing elit. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Cursus in hac habitasse platea dictumst quisque sagittis purus `,
+                initialUpvotes: 18000,
+            },
+            {
+                id: 5,
+                content: `Lorem5 ipsum dolor sit amet, consectetur adipiscing elit. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Cursus in hac habitasse platea dictumst quisque sagittis purus `,
+                initialUpvotes: 18000,
+            }
+        ],
+        commentBox: {
+            content: "",
+            onChange: (value: string) => { console.log(value); },
+            send: <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#1f1f1f"><path d="M0 0h24v24H0z" fill="none"/><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/></svg>
+        },
+        numberOfComments: 5,
+        backButton: <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#1f1f1f"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+    },
+};
+
+export const Outlined: Story = {
+    args: {
+        variant: "outlined",
+        comments: [
+            {
+                id: 1,
+                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Cursus in hac habitasse platea dictumst quisque sagittis purus `,
+                initialUpvotes: 18000,
+            },
+            {
+                id: 1,
+                content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Cursus in hac habitasse platea dictumst quisque sagittis purus `,
+                initialUpvotes: 18000,
+            }
+        ],
+        commentBox: {
+            content: "",
+            onChange: (value: string) => { console.log(value); },
+            send: <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#1f1f1f"><path d="M0 0h24v24H0z" fill="none"/><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/></svg>,
+        },
+        numberOfComments: 2,
+        backButton: <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#1f1f1f"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+    },
+};
