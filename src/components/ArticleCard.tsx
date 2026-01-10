4692270f536c3b698aeede92742c74bf4d276b52
Awesome-Tiya <tiyashidas123@gmail.com>
2026-01-10 13:32:06 +0530

    updates

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -20,17 +20,17 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({ children, title, varia
     } as React.CSSProperties;
 
     const upvoteNum = Number.isFinite(upvotes) ? upvotes : 0;
-    const formattedUpvotes = new Intl.NumberFormat('en', {
-        notation: "compact",
-        compactDisplay: "short",
-        maximumFractionDigits: 2
-    }).format(upvoteNum);
+    // const formattedUpvotes = new Intl.NumberFormat('en', {
+    //     notation: "compact",
+    //     compactDisplay: "short",
+    //     maximumFractionDigits: 2
+    // }).format(upvoteNum);
     return (<>
         <article className={`article-card ${variant}`} style={style}>
             <div className="upvote-line">
                 <button type="button" className="upvotes" onClick={onUpvote}>
                     <img src={upvote} alt="flower"/>
-                    <span className="upvote-count">{ formattedUpvotes }</span>
+                    <span className="upvote-count">{ upvoteNum }</span>
                 </button>
                 <h3 className="title">{ title }</h3>
                 {prev && <button type="button" className="prev" aria-label="previous" onClick={onPrev}>◀ prev</button>}
